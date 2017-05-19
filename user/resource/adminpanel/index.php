d5796946ec428a8c2748fa581e00dffadc7978b0
Kamil Owczarz <kamilo2602@gmail.com>
2017-05-19 12:21:03 +0200

    gitignore kolejny raz zmodyfikowane

---
+++ user/resource/adminpanel/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Panel administratora</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <script src="../angular/angular.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
