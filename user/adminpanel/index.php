aae5418bf64e32cd45f9ea0554527f6ce98f5254
Kamil Owczarz <kamilo2602@gmail.com>
2017-05-19 18:31:46 +0200

    Kurwa, najlepsze dziś.

--- user/adminpanel/index.php
+++ user/adminpanel/index.php
@@ -9,7 +9,6 @@
 </head>
 <body>
 <?php
-session_start();
 if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
     header("Location: logged.php");
     exit();
--- user/adminpanel/logged.php
+++ user/adminpanel/logged.php
@@ -9,7 +9,6 @@
 </head>
 <body>
 <?php
-session_start();
 if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
     header("Location: index.php");
     exit();
