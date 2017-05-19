07d749ccb429da48bbf084c6d2118c0cc6cdcd8c
Kamil Owczarz <kamilo2602@gmail.com>
2017-05-19 23:03:06 +0200

    Dodany navbar na szablonie podstrony, nie działa zapadanie, ogarnij TODO

--- user/adminpanel/logged.php
+++ user/adminpanel/logged.php
@@ -19,22 +19,17 @@
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="index.php">Nazwa serwera</a> <!-- TODO funkcja podająca nazwę serwera -->
+    <a class="navbar-brand ml-lg-2" href="index.php">Nazwa serwera</a> <!-- TODO funkcja podająca nazwę serwera -->
     <div class="collapse navbar-collapse">
-        <ul>
-            <li><a href="#" class="nav-link">cośtam kurwa</a></li>
+        <ul class="navbar-nav mr-auto mt-2 mt-md-0 ml-lg-5">
+            <!-- TODO na ile działów i jakie dzielimy admin panel? -->
+            <li class="nav-item"><a href="" class="nav-link">kutas</a></li>
+            <li class="nav-item"><a href="" class="nav-link">beniz</a></li>
+            <li class="nav-item"><a href="" class="nav-link">chuj</a></li>
+            <li class="nav-item"><a href="" class="nav-link">ci w dupę</a></li>
         </ul>
-        <ul>
-            <li><a href="#" class="nav-link">Cośtam 2</a></li>
-        </ul>
-        <ul>
-            <li><a href="#" class="nav-link">Kurwa 3</a></li>
-        </ul>
-        <ul>
-            <li><a href="#" class="nav-link">No ja jebię 4</a></li>
-        </ul>
-        <ul>
-            <li><a href="#" class="nav-link">No i chuj</a></li>
+        <ul class="navbar-nav mt-2 mt-md-0 mr-lg-2">
+            <li class="nav-item"><a href="#" class="btn btn-outline-secondary">wyloguj</a></li>
         </ul>
     </div>
 </nav>
